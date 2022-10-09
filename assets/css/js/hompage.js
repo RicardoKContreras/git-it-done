d57f3fbd13e086d65bb5b61af50b6408c878c14b
Jimbei <contrerasr2272@gmail.com>
2022-10-08 20:41:10 -0400

    setting up fetch api

--- assets/css/js/hompage.js
+++ assets/css/js/hompage.js
@@ -1,5 +1,24 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+//*Note how we've added a parameter to the getUserRepos() function and inserted the parameter into the GitHub API URL. We then use the newly formatted URL in the subsequent fetch() request.
+var getUserRepos = function(user) {
+
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+
+    //* first time using fetch
+//     fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+//         response.json().then(function(data) {
+//             console.log(data);
+//           });
+// });
+
+// console.log("outside");
 };
 
-getUserRepos();
+getUserRepos("microsoft");
